b387629bea75656b145f559f7c9c8703f0827c4a
Deep Muni <deep.muni94@gmail.com>
2024-12-06 16:41:29 +0530

    Adds test to convert 5 to Buzz

--- src/main/java/com/learning/kata/FizzBuzz.java
+++ src/main/java/com/learning/kata/FizzBuzz.java
@@ -2,8 +2,11 @@ package com.learning.kata;
 
 public class FizzBuzz {
   public String convert(Integer value) {
-    if(value.equals(3)){
+    if (value.equals(3)) {
       return "Fizz";
+    }
+    if (value.equals(5)) {
+      return "Buzz";
     }
     return value.toString();
   }
--- src/test/java/com/learning/kata/FizzBuzzTest.java
+++ src/test/java/com/learning/kata/FizzBuzzTest.java
@@ -2,25 +2,35 @@ package com.learning.kata;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
 
+  private static FizzBuzz fizzBuzz;
+
+  @BeforeAll
+  static void beforeAll() {
+    fizzBuzz = new FizzBuzz();
+  }
+
   @Test
   void convert_1_to_1() {
-    FizzBuzz fizzBuzz = new FizzBuzz();
     assertEquals("1", fizzBuzz.convert(1));
   }
 
   @Test
   void convert_2_to_2() {
-    FizzBuzz fizzBuzz = new FizzBuzz();
     assertEquals("2", fizzBuzz.convert(2));
   }
 
   @Test
-  void convert_3_to_3() {
-    FizzBuzz fizzBuzz = new FizzBuzz();
+  void convert_3_to_Fizz() {
     assertEquals("Fizz", fizzBuzz.convert(3));
+  }
+
+  @Test
+  void convert_5_to_Buzz() {
+    assertEquals("Buzz", fizzBuzz.convert(5));
   }
 }
